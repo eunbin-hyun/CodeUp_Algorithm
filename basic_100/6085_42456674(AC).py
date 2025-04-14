cfbee460c87676249a1b2bb2d62edcd7bab54687
eunbin-hyun <eunbin8592@gmail.com>
2025-04-14 16:05:55 +0900

    print

---
+++ basic_100/6085_42456674(AC).py
@@ -0,0 +1,3 @@
+﻿w, h, b = map(float, input().split())
+k = (w*h*b)/(8*1024*1024)
+print(f"{k:.2f} MB")
---
+++ basic_100/6086_42458452(AC).py
@@ -0,0 +1,11 @@
+﻿n = int(input())
+s = 0
+c = 0
+
+while True:
+    c += 1
+    s += c
+    if s >= n:
+        break
+    
+print(s)
