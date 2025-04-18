167e6b9ed4fdbece43953f4cfd5c4654381087a6
eunbin-hyun <eunbin8592@gmail.com>
2025-04-18 13:49:56 +0900

    계산

---
+++ basic_100/6088_42569548(AC).py
@@ -0,0 +1,10 @@
+﻿a,d,n = map(int, input().split())
+k=0
+s=0
+
+while True:
+    k = a + s*d
+    s += 1
+    if s == n:
+        print(k)
+        break
---
+++ basic_100/6089_42569565(AC).py
@@ -0,0 +1,10 @@
+﻿a,r,n = map(int, input().split())
+k=0
+s=0
+
+while True:
+    k = a*(r**s)
+    s += 1
+    if s == n:
+        print(k)
+        break
