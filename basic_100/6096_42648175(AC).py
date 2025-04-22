8a20b5040a15ff02a5d094e5aa24b7340c50ec00
eunbin-hyun <eunbin8592@gmail.com>
2025-04-22 18:25:12 +0900

    바둑판

---
+++ basic_100/6096_42648175(AC).py
@@ -0,0 +1,26 @@
+﻿d = []
+for _ in range(19):
+    row = list(map(int, input().split()))
+    d.append(row)
+
+n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
+    x -= 1
+    y -= 1
+    for j in range(19):
+        if d[x][j] == 0:
+            d[x][j] = 1
+        else:
+            d[x][j] = 0
+            
+    for k in range(19):
+        if d[k][y] == 0:
+            d[k][y] = 1
+        else:
+            d[k][y] = 0
+            
+for i in range(19):
+    for j in range(19):
+        print(d[i][j], end= ' ')
+    print()
