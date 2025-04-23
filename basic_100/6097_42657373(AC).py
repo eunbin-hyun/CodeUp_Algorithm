560dacbcf8ddc968e49d1bb4f0cdb0d0367d231d
eunbin-hyun <eunbin8592@gmail.com>
2025-04-23 16:01:10 +0900

    길찾기

---
+++ basic_100/6097_42657373(AC).py
@@ -0,0 +1,20 @@
+﻿h, w = map(int, input().split())
+n = int(input())
+d = [[0 for _ in range(w)] for _ in range(h)]
+
+for _ in range(n):
+    i, d_dir, x, y = map(int, input().split())
+    x -= 1 
+    y -= 1 
+    
+    if d_dir == 0:
+        for l in range(i):
+            d[x][y+l] = 1
+    else:
+        for l in range(i):
+            d[x+l][y] = 1
+            
+for i in range(h):
+    for j in range(w):
+        print(d[i][j], end=' ')
+    print()
---
+++ basic_100/6098_42661312(AC).py
@@ -0,0 +1,30 @@
+﻿m = [[0 for _ in range(10)] for _ in range(10)]
+
+for i in range(10):
+    m[i] = list(map(int, input().split()))
+
+a = 1 
+b = 1
+m[1][1] = 9
+
+while True:
+    if m[a][b+1] == 2:
+        m[a][b+1] = 9
+        break
+    elif m[a][b+1] == 1 and m[a+1][b] == 2:
+        m[a+1][b] = 9
+        break
+    
+    if m[a][b+1] == 0:
+        m[a][b+1] = 9
+        b += 1 
+    elif m[a][b+1] == 1 and m[a+1][b] == 0:
+        m[a+1][b] = 9
+        a += 1
+    else:
+        break
+    
+for i in range(10):
+    for j in range(10):
+        print(m[i][j], end= ' ')
+    print()
